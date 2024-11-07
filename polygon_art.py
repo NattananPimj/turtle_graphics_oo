c6d442f397e7dbe5e0c9ba1ccfffe3fa5045b831
Nattanan <nattanan.pim@ku.th>
2024-11-07 14:18:12 +0700

    change to oop

--- polygon_art.py
+++ polygon_art.py
@@ -1,52 +1,109 @@
 import turtle
 import random
 
-def draw_polygon(num_sides, size, orientation, location, color, border_size):
-    turtle.penup()
-    turtle.goto(location[0], location[1])
-    turtle.setheading(orientation)
-    turtle.color(color)
-    turtle.pensize(border_size)
-    turtle.pendown()
-    for _ in range(num_sides):
-        turtle.forward(size)
-        turtle.left(360/num_sides)
-    turtle.penup()
+class Polygon:
+    def __init__(self, num_sides: int = 3, size: float = 1, orientation: float = 0, location_x=0, location_y=0,
+                 color: tuple[int, int, int] = (0, 0, 0), border_size: int = 1):
+        self.num_sides = num_sides
+        self.size = size
+        self.orientation = orientation
+        self.x = location_x
+        self.y = location_y
+        self.color = color
+        self.border_size = border_size
+        self.num = 1
+        self.reduction_ratio = .618
 
-def get_new_color():
-    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    def draw_polygon(self):
+        turtle.penup()
+        turtle.goto(self.x, self.y)
+        turtle.setheading(self.orientation)
+        turtle.color(self.color)
+        turtle.pensize(self.border_size)
+        turtle.pendown()
+        for _ in range(self.num_sides):
+            turtle.forward(self.size)
+            turtle.left(360 / self.num_sides)
+        turtle.penup()
 
-turtle.speed(0)
-turtle.bgcolor('black')
-turtle.tracer(0)
-turtle.colormode(255)
+    def draw_smaller(self):
+        for _ in range(2):
+            self.size *= self.reduction_ratio
+            turtle.forward(self.size * (1 - self.reduction_ratio) / 2)
+            turtle.left(90)
+            turtle.forward(self.size * (1 - self.reduction_ratio) / 2)
+            turtle.right(90)
+            self.x = turtle.pos()[0]
+            self.y = turtle.pos()[1]
+            self.draw_polygon()
 
-# draw a polygon at a random location, orientation, color, and border line thickness
-num_sides = random.randint(3, 5) # triangle, square, or pentagon
-size = random.randint(50, 150)
-orientation = random.randint(0, 90)
-location = [random.randint(-300, 300), random.randint(-200, 200)]
-color = get_new_color()
-border_size = random.randint(1, 10)
-draw_polygon(num_sides, size, orientation, location, color, border_size)
+    def setup(self):
+        turtle.speed(0)
+        turtle.bgcolor('black')
+        turtle.tracer(0)
+        turtle.colormode(255)
 
-# specify a reduction ratio to draw a smaller polygon inside the one above
-reduction_ratio = 0.618
 
-# reposition the turtle and get a new location
-turtle.penup()
-turtle.forward(size*(1-reduction_ratio)/2)
-turtle.left(90)
-turtle.forward(size*(1-reduction_ratio)/2)
-turtle.right(90)
-location[0] = turtle.pos()[0]
-location[1] = turtle.pos()[1]
+class Run(Polygon):
+    def run_choice(self):
+        choice = int(input('Which art do you want to generate? Enter a number between 1 to 9 inclusive:'))
+        if choice == 1:
+            self.run(3)
+            turtle.done()
+        elif choice == 2:
+            self.run(3)
+            turtle.done()
+        elif choice == 3:
+            self.run(5)
+            turtle.done()
+        elif choice == 4:
+            self.run(3, (3, 12))
+            self.run(4, (3, 12))
+            self.run(5, (3, 12))
+            turtle.done()
+        elif choice == 5:
+            self.run(3, t=1)
+            turtle.done()
+        elif choice == 6:
+            self.run(4, t=1)
+            turtle.done()
+        elif choice == 7:
+            self.run(5, t=1)
+            turtle.done()
+        elif choice == 8:
+            self.run(3, (3, 12), t=1)
+            self.run(4, (3, 12), t=1)
+            self.run(5, (3, 12), t=1)
+            turtle.done()
+        elif choice == 9:
+            self.run(3, (1, 10))
+            self.run(4, (1, 10))
+            self.run(5, (1, 10))
+            self.run(3, (1, 10), t=1)
+            self.run(4, (1, 10), t=1)
+            self.run(5, (1, 10), t=1)
+            turtle.done()
 
-# adjust the size according to the reduction ratio
-size *= reduction_ratio
+    def random_input(self):
+        self.size = random.randint(50, 150)
+        self.orientation = random.randint(0, 90)
+        self.x = random.randint(-300, 300)
+        self.y = random.randint(-200, 200)
+        self.color = get_new_color()
+        self.border_size = random.randint(1, 10)
 
-# draw the second polygon embedded inside the original 
-draw_polygon(num_sides, size, orientation, location, color, border_size)
+    def run(self, sides: int, num_range=(20, 40), t: int = 0):
+        self.num_sides = sides
+        self.num = random.randint(num_range[0], num_range[1])
+        self.setup()
+        for _ in range(self.num):
+            self.random_input()
+            self.draw_polygon()
+            if t == 1:
+                self.draw_smaller()
 
-# hold the window; close it by clicking the window close 'x' mark
-turtle.done()
+
+
+
+art = Run()
+art.run_choice()
